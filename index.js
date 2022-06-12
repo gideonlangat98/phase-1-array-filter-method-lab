aa4da3f565ad81f86f7740e3363e1ad10bece2f7
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-12 23:47:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = [
+    {
+        name: "Bobby"
+    },
+    {
+        name: "Sammy"
+    },
+    {
+        name: "Sally"
+    },
+    {
+        name: "Annette"
+    },
+    {
+        name: "Sarah"
+    },
+    {
+        name: "Bobby"
+    }
+];
+
+function findMatching(driver, name) {
+    return driver.filter((Matching) => Matching.toLowerCase() === name.toLowerCase());
+};
+
+function fuzzyMatch(driver, fuzzy) {
+    return driver.filter(
+        (Matching) => Matching[0] === fuzzy[0]);  
+}
+
+function matchName(driver, sortingName) {
+    return driver.filter((record) => record.name === sortingName);
+
+}
